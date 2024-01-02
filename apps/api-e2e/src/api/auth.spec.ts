6c75f5094a8205a4d3de1cb25e0105b3a670468a
RahafAl0 <ra.al9760@gmail.com>
2024-01-02 15:33:21 +0300

    add login test

--- apps/api-e2e/src/api/auth.spec.ts
+++ apps/api-e2e/src/api/auth.spec.ts
@@ -4,14 +4,36 @@ import { error } from 'console';
 describe('POST: /api/register', () => {
   it('should create new user', async () => {
     const response = await axios.post('/api/register', {
-      username: 'rahaaafRiyad',
-      email: 'rahafrahaffffff@rahaf',
+      username: 'rahaaafrrrReeiyad',
+      email: 'rahafrahaffffffrrreer@rahaf',
       password: '123Rahaf',
     });
 
     expect(response.status).toBe(200);
-    expect(response.data.id).toEqual(4);
+    expect(response.data.id).toEqual(7);
     error('Error creating user:', error);
   });
   
 });
+
+describe('POST: /api/login', () => {
+  it('should login with valid credentials', async () => {
+    const response = await axios.post('/api/login', {
+      username: 'rahaaafRiyad',
+      password: '123Rahaf',
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.data.token).toBeDefined();
+  });
+
+  it('should handle login error with invalid credentials', async () => {
+    await expect(
+      axios.post('/api/login', {
+        username: 'nonexistentUser',
+        password: 'invalidPassword',
+      })
+    ).rejects.toThrowError();
+  });
+});
+
