219dc47f890aa4320a4b4f4bc0b60bcf21fd4197
RahafAl0 <ra.al9760@gmail.com>
2024-01-20 15:44:23 +0300

    add update endpoint for articles

--- apps/api/src/articles.ts
+++ apps/api/src/articles.ts
@@ -6,24 +6,63 @@ const prisma = new PrismaClient();
 const articleRouter = express.Router();
 
 articleRouter.post('/articles', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    const { title, content } = req.body;
-    const userId = (req.user as { id: string }).id;
+  const { title, content } = req.body;
+  const userId = (req.user as { id: string }).id;
 
-    try {
-      const newArticle = await prisma.article.create({
-        data: {
-          title,
-          content,
-          author: { connect: { id: Number(userId) } },
-        },
-      });
+  try {
+    const newArticle = await prisma.article.create({
+      data: {
+        title,
+        content,
+        author: { connect: { id: Number(userId) } },
+      },
+    });
 
-      res.status(201).json({ message: 'Article created successfully', article: newArticle });
-    } catch (error) {
+    res.status(201).json({  });
+  } catch (error) {
       console.error('Error creating article:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   }
 );
+
+articleRouter.get('/articles', async (req, res) => {
+  try {
+    const articles = await prisma.article.findMany({
+      include: {
+        author: false, 
+      },
+    });
+
+    res.status(200).json(articles);
+  } catch (error) {
+      console.error('Error fetching articles:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+articleRouter.put('/articles/:id', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  const { id } = req.params;
+  const numericId = parseInt(id);
+
+  const { title, content } = req.body;
+
+  try {
+    const updatedArticle = await prisma.article.update({
+      where: { id: numericId },
+      data: {
+        title,
+        content,
+      },
+    });
+
+    res.status(200).json({ message: 'Article updated successfully', article: updatedArticle });
+  } catch (error) {
+    console.error('Error updating article:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 
 export default articleRouter;
