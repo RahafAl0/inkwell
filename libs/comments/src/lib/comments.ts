550142e0678b7c1dc596fd661a314225c9fa8fa5
RahafAl0 <ra.al9760@gmail.com>
2024-02-10 00:29:27 +0300

    DELETE comment

--- libs/comments/src/lib/comments.ts
+++ libs/comments/src/lib/comments.ts
@@ -35,7 +35,27 @@ commentRouter.get('/comments', async (req, res) => {
     console.log('Error fetching comments');
     res.status(500).json({ error: 'Internal Server Error' })
   }
-})
+});
+
+commentRouter.get('/comments/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const numericId = parseInt(id);
+
+    const comment = await prisma.comment.findUnique({
+      where: { id: numericId },
+    });
+
+    if (!comment) {
+      return res.status(404).json({ error: 'Comment not found' });
+    }
+    res.status(200).json(comment);
+  } catch (error) {
+    console.error('Error fetching comment:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 commentRouter.put('/comments/:id', passport.authenticate('jwt', { session: false }), async (req, res) => {
   const { id } = req.params;
@@ -49,10 +69,30 @@ commentRouter.put('/comments/:id', passport.authenticate('jwt', { session: false
     });
     res.status(200).json({ message: 'Commment Update Successfully', comment: updateComment });
   } catch (error) {
-    console.log('Error Updating Cooment', error);
-    res.status(500).json({ error: 'Internal Server Error' })
+    console.log('Error Updating Comment', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
+});
 
-})
+commentRouter.delete('/comments/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const numericId = parseInt(id);
+
+    const deletedComment = await prisma.comment.delete({
+      where: { id: numericId },
+    });
+
+    if (!deletedComment) {
+      return res.status(404).json({ error: 'Comment not found' });
+    }
+
+    res.status(200).json({ message: 'Comment deleted successfully', deletedComment });
+  } catch (error) {
+    console.error('Error deleting comment:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 export { commentRouter };
