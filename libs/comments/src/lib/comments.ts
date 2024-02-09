8f4665f4477580485c64a84ec133ea776adce60b
RahafAl0 <ra.al9760@gmail.com>
2024-02-09 14:14:11 +0300

    user now can update comment

--- libs/comments/src/lib/comments.ts
+++ libs/comments/src/lib/comments.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import passport from 'passport';
 
 
 const prisma = new PrismaClient();
@@ -17,11 +19,29 @@ commentRouter.post('/comments', async (req, res) => {
       },
     });
 
-    res.status(201).json({ comment: newComment });
+    res.status(201).json({  });
   } catch (error) {
     console.log('Error', error);
     res.status(500).json({ error: "Internal Server Error" })
   }
 });  
+
+commentRouter.put('/comments/:id', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  const { id } = req.params;
+  const { content } = req.body
+
+  try {
+    const numericId = parseInt(id)
+    const updateComment = await prisma.comment.update({
+      where: { id: numericId },
+      data: { content }
+    });
+    res.status(200).json({ message: 'Commment Update Successfully', comment: updateComment });
+  } catch (error) {
+    console.log('Error Updating Cooment', error);
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+
+})
 
 export { commentRouter };
