ae65d4a6523a5cebfd4bfede000cb689a3198976
RahafAl0 <ra.al9760@gmail.com>
2024-02-21 15:49:05 +0300

    Register & RTK query

--- apps/inkwell/src/main.tsx
+++ apps/inkwell/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import './styles.css'
-import App from './app/app';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./app/app";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>  );
+
+}
--- apps/inkwell/src/store/index.tsx
+++ apps/inkwell/src/store/index.tsx
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice";
+import authApi from "./apis/authApi";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    auth: authReducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
+
+export default store;
