bac0c15aeec9cbb1f7018c0d8d865af8dfb13503
RahafAl0 <ra.al9760@gmail.com>
2024-02-27 23:15:09 +0300

    add MyProfile

---
+++ apps/inkwell/src/components/MyProfile.tsx
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+import Article from './Articles';
+
+function MyProfile() {
+
+  
+
+  return (
+    <div>
+      <Navbar />
+      <Article />
+    </div>
+  );
+}
+
+export default MyProfile;
