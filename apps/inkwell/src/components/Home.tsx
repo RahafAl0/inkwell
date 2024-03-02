e563b5cf7cf744ed736e47bde2a5e9baf955d7c0
RahafAl0 <ra.al9760@gmail.com>
2024-03-02 18:20:39 +0300

    make published article appear in home page

--- apps/inkwell/src/components/Home.tsx
+++ apps/inkwell/src/components/Home.tsx
@@ -1,13 +1,39 @@
-// import Article from './Articles';
 import Navbar from './Navbar';
-// import Comments from './Comments';
+import { useGetArticlesQuery } from '../store/apis/articleApi';
+import { Article } from './Types';
 
 function Home() {
+  const { data: articles, isLoading } = useGetArticlesQuery({});
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className="bg-red-100 min-h-screen flex items-center justify-center">
       <Navbar />
-      {/* <Article /> */}
-      {/* <Comments comments={[]} /> */}
+      <div>
+        <h1 className="text-2xl font-semibold mb-4">Articles</h1>
+        <div>
+          {articles.map((article: Article) => (
+            <div
+              key={article.id}
+              className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-4"
+            >
+              <div className="md:flex">
+                <div className="p-8">
+                  <p className="mt-2 text-gray-500">
+                    {article.author.username} 
+                  </p>
+                  <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                    {article.title}
+                  </div>
+                  <p className="mt-2 text-gray-500">{article.content}</p>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
